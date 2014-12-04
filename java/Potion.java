5699382ba8bde7d1e30c9f9bf7a011c9a9a4474c
elisa <elisamarcatto>
2014-12-04 11:36:13 -0500

    class potion modified to interface potion

--- java/Potion.java
+++ java/Potion.java
@@ -1,36 +1,9 @@
-import java.util.*;
+interface Potion{
 
-public abstract class Potion extends Item{
+	public int getRestorePts();
+// mudar ou nao para manapts
 
-	private int restorepts;
-
-	public Potion(String name, double price, int restorepts){
-		super(name, price);
-
-		this.restorepts = restorepts;
-
-	}
-
-	public Potion clone() {
-		return (Potion)super.clone();
-	}
-
-	public int getRestorePts(){
-		return restorepts;
-	}
-
-	public int getAttackPts(){ 
-		return 0;
-	}
-
-	public int getDefensePts(){ 
-		return 0;
-	}
-	// usa a pocao e depois deleta ela
-	public void use(Character character){ }	
-	// nao eh utilizado nessa classe
-	public void equip(Character character){ }	
-	public void unequip(Character character){ }	
-
+	// Indica uso dos itens 'potion'(implementado em ManaPotion e HealthPotion)
+	public void use(Character character);
 
 }
